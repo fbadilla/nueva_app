748b68e35caf142c438f73463f898c912d79fcca
fbadilla <f.badilla228@gmail.com>
2021-02-09 14:16:23 -0300

    integracion firebase

--- src/config/firebase.config.jsx
+++ src/config/firebase.config.jsx
@@ -4,14 +4,13 @@
  */
 
 const FIREBASE = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://PROJECT_ID.firebaseio.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
-  measurementId: "G-MEASUREMENT_ID"
+  apiKey: "AIzaSyC6Ui_9Tw1X-RKalRPnfaYOH_3Jwqs5Boo",
+  authDomain: "isanitaria-95d8c.firebaseapp.com",
+  projectId: "isanitaria-95d8c",
+  storageBucket: "isanitaria-95d8c.appspot.com",
+  messagingSenderId: "88888658365",
+  appId: "1:88888658365:web:67202836a83d4e9b706ad4",
+  measurementId: "G-D0G4PKKBRB"
 }
 
 export default FIREBASE
